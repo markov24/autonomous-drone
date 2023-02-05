49f676001037512cbe8e9500a7457b8b7b168c47
Marko Veljanovski <markoveljanovski2025@u.northwestern.edu>
2023-02-05 15:51:14 -0600

    updating box detection script to use newest box model

--- detection-scripts/ssd-box.py
+++ detection-scripts/ssd-box.py
@@ -1,7 +1,7 @@
 import jetson_inference
 import jetson_utils
 
-net = jetson_inference.detectNet(model="/home/drone/autonomous-drone/models/box-test/ssd-mobilenet.onnx", labels="/home/drone/autonomous-drone/models/box-test/labels.txt", input_blob="input_0", output_cvg="scores", output_bbox="boxes", threshold=0.7)
+net = jetson_inference.detectNet(model="/home/drone/autonomous-drone/models/box_custom/ssd-mobilenet.onnx", labels="/home/drone/autonomous-drone/models/box_custom_basic/labels.txt", input_blob="input_0", output_cvg="scores", output_bbox="boxes", threshold=0.5)
 camera = jetson_utils.videoSource("/dev/video0")
 display = jetson_utils.videoOutput()
 
